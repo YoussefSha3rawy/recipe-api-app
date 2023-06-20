18e6c5deea48bc4bdc8a2ef9a9f2dfab9c2405ca
Youssef Shaarawy <youssefshaarawy@gmail.com>
2023-06-20 16:39:11 +0300

    Added core, user and recipe apps Add auth Added serializers and tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,10 +1,12 @@
 """
 Tests for models.
 """
+from decimal import Decimal
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 from faker import Faker
+from core import models
 
 
 class ModelTests(TestCase):
@@ -43,9 +45,25 @@ class ModelTests(TestCase):
     def test_create_superuser(self):
         """Test creating a superuser."""
         user = get_user_model().objects.create_superuser(
-            'test@example.com',
-            'test123',
+            self.fake_data_generator.email(),
+            self.fake_data_generator.password(),
         )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful."""
+        user = get_user_model().objects.create_user(
+            self.fake_data_generator.email(),
+            self.fake_data_generator.password(),
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Sample receipe description.',
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
